b6f2c7a4eb35aa59a76f15da58e82d72f51ea91f
Polina <paulinachernysh@gmail.com>
2025-04-03 21:39:14 +0300

    task 2  js

--- src/Cycles.js
+++ src/Cycles.js
@@ -3,18 +3,46 @@
   Используя цикл for, просуммируйте все четные числа в диапазоне между этими значениями (включительно)
   и верните итоговый результат.
 */
-export function rangeSum(start, end) {}
+export function rangeSum(start, end) {
+  let sum = 0;
+  for (let i = start; i <= end; i++) {
+    if (i % 2 === 0) {
+      sum += i;
+    }
+  }
+  return sum;
+}
 
 /*
   В функцию iterationCount() приходит неотрицательное число.
   Используя цикл while, выполняйте деление этого числа на два до тех пор, пока результат деления больше 0.1
   и верните количество потребовавшихся итераций (т.е. сколько раз пришлось выполнить деление).
 */
-export function iterationCount(a) {}
+export function iterationCount(a) {
+  let mod = a
+  let i = 0
+  while (mod>0.1) {
+    mod = mod/2
+    i++
+  }
+  return i
+}
 
 /*
   В функцию symbolsReplace() приходит строка текста.
   Используя цикл do while, замените в тексте каждый третий символ на символ нижнего подчеркивания
   и верните итоговый результат.
 */
-export function symbolsReplace(message) {}
+export function symbolsReplace(message) {
+  let newMess = String(message);
+  let i = 2;
+  do {
+    if (i < newMess.length) {
+      newMess = newMess.substring(0, i) + '_' + newMess.substring(i + 1);
+      i += 3;
+    } else {
+      break;
+    }
+  } while (true);
+  return newMess;
+}
